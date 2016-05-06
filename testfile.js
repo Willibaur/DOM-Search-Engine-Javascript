9021dcde5a6d1f2060755ad8894a85161568aebb
William Bautista <williambautista@gmail.com>
2016-05-06 09:46:39 +0100

    Total hours and money paid fixed

--- testfile.js
+++ testfile.js
@@ -1,7 +1,6 @@
 var btn = document.getElementById("get-records");
 btn.addEventListener('click', buttonHandler);
 
-
 function buttonHandler () {
   console.log("You have clicked");
   toggleButton(false);
@@ -9,14 +8,13 @@ function buttonHandler () {
 }
 
 function toggleButton (loaded) {
-  // fix this method to change the button text
   btn.innerHTML = loaded ? "Get next" : "Loading...";
   btn.classList.toggle("button-not-loading");
   btn.classList.toggle("button-loading");
 }
 
 function getRecords () {
-  var ids = Server.getIds();
+  var ids           = Server.getIds();
   var allTheRecords = [];
 
   ids.forEach(function(recordId) {
@@ -29,20 +27,6 @@ function getRecords () {
       }
     });
   });}
-  // getting each corresponding record is an async operation
-  // you need to make sure the list is not rendered until we have all the records!
-  // you can get a SINGLE record by calling Server.getRecord(recordId, callbackFunction)
-  // callbackFunction takes 2 parameters, error and data
-  // the invocation will look like this
-
-  // if the fetch is unsuccessful the callback function is invoked with the error only
-  // if the fetch is successful the callback is invoked with error set to null, and data will holds the response (i.e. the record you wanted to retrieve)
-  // the tricky thing (and what we are assessing) is how to wait for ALL the callbacks to complete before you process the responses?
-  // HINT - you know how many times you need to call Server.getRecord before you do it...
-  // ...meaning that you have a way of tracking how many have "called back"
-  // when you have all the records, call processRecords as follows
-
-
 
 function processRecords (records) {
   toggleButton(true);
@@ -67,9 +51,9 @@ function processRecords (records) {
 }
 
 function sortRecords (records) {
-  var sorted = records.sort(function(currentValue, nextValue) {
+  var sorted         = records.sort(function(currentValue, nextValue) {
     currentValueDate = parseDate(currentValue.date);
-    nextValueDate = parseDate(nextValue.date);
+    nextValueDate    = parseDate(nextValue.date);
 
     return currentValueDate - nextValueDate;
   });
@@ -83,18 +67,20 @@ function parseDate(strDate) {
 }
 
 function addTotals (records) {
-
   var hours = 0;
-  var paid = 0;
+  var paid  = 0;
 
-  // you get brownie points for fixing the total hours
-
-  records.forEach(function (value, index) {
-    hours += value.hoursWorked;
-    paid += (value.hoursWorked * value.hourlyRate);
+   records.forEach(function (element) {
+    hoursWorked = parseHours(element.hoursWorked);
+    hours      += hoursWorked;
+    paid       += hoursWorked * element.hourlyRate;
   });
 
   document.getElementById("totals-annot").innerHTML = "TOTALS";
   document.getElementById("totals-hours").innerHTML = hours;
   document.getElementById("totals-paid").innerHTML = paid;
+}
+
+function parseHours(strHours) {
+  return parseInt(strHours);
 }
